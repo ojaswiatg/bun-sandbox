40a4d0372b011a2e644ab7ff5d3c09e4d871d308
Ojaswi Athghara <ojaswi@acalvio.com>
2024-09-23 12:39:50 +0530

    box init

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        rules: {
+            "prefer-const": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                },
+            ],
+            curly: ["error", "all"],
+            "no-console": [
+                "error",
+                {
+                    allow: ["warn", "error", "info"],
+                },
+            ],
+        },
+    },
+];
